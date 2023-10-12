21f24fa2a043a6fbb502b3d7927212b085437da8
mbageri <mohammadbageri@outlook.com>
2023-10-13 01:33:20 +0200

    bugfix
    bugfix

--- git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "1" >> 1.txt
-git add .
-
-for num in $(seq 270 500 | shuf | tail -n1);do
-	git commit --date="$num day ago" -m "bugfix"
-done
-
-echo "---------commit done in $num day's ago!------------"
